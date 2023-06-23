e15c5360892f11e2225edee829555020a37eeaa9
Esdras <josuequeraleswd2420@gmail.com>
2023-06-23 16:47:41 -0430

    feat: Agregado Controlador para Engorde

---
+++ app/Http/Controllers/Bovino/EngordeController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\Bovino;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\GanadoController;
+use App\Models\Engorde;
+use App\Models\Ganado;
+use App\Http\Requests\Bovino\StoreEngordeRequest;
+use App\Http\Requests\Bovino\UpdateEngordeRequest;
+use Illuminate\Http\Request;
+
+class EngordeController extends Controller
+{
+    private $base;
+
+    public function __construct(GanadoController $ganado)
+    {
+        $this->base = $ganado;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEngordeRequest $request, Engorde $engorde)
+    {
+        //
+        $ganado = $this->base->store($request, new Ganado());
+
+        if($ganado->id){
+            $engorde->ganado_id = $ganado->id;
+
+            $engorde->tipo_alimento = $request->tipo_alimento;
+            $engorde->cant_total_alimento = $request->cant_total_alimento;
+            $engorde->duracion =  $request->duracion;
+            $engorde->peso_inicial = $request->peso_inicial;
+            $engorde->peso_final = $request->peso_final;
+
+            if($engorde->save()){
+                return redirect()->route('engorde.show', ['engorde' => $engorde->id]);
+            }
+        }
+
+        //Error presente, si se alcanza este punto
+        return back()->withErrors([
+            'status' => 'No fue posible realizar el registro'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Engorde $engorde)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Engorde $engorde)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Engorde $engorde)
+    {
+        $ganado = $this->base->update($request, $engorde->ganado);
+
+        $engorde->tipo_alimento = $request->tipo_alimento;
+        $engorde->cant_total_alimento = $request->cant_total_alimento;
+        $engorde->duracion =  $request->duracion;
+        $engorde->peso_inicial = $request->peso_inicial;
+        $engorde->peso_final = $request->peso_final;
+
+        if($engorde->save()){
+            return redirect()->route('engorde.show', ['engorde' => $engorde->id]);
+        }
+
+        //Error presente, si se alcanza este punto
+        return back()->withErrors([
+            'status' => 'No fue posible guardar los datos'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Engorde $engorde)
+    {
+        //
+        $engorde->ganado->delete();
+        $engorde->delete();
+
+        return redirect()->route('engorde.index')->withInput([
+            'status' => 'Registro eliminado exitosamente'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,8 @@ Route::controller(App\Http\Controllers\AuthController::class)->group(function ()
         
         Route::resources([
             'vaca' => App\Http\Controllers\Bovino\MadreController::class,
-            'cria' => App\Http\Controllers\Bovino\CriaController::class
+            'cria' => App\Http\Controllers\Bovino\CriaController::class,
+            'engorde' => App\Http\Controllers\Bovino\EngordeController::class
         ]);
         
     });
