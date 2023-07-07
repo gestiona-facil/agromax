541a46c2316a1021b109bd700faf5632f1718761
Esdras <josuequeraleswd2420@gmail.com>
2023-07-07 03:26:43 -0430

    feat: agregado header y sidebar en vistas de agricultura

---
+++ resources/views/agricultura/maiz/layout/header.blade.php
@@ -0,0 +1,15 @@
+<header class="bg-gradient-to-r from-green-700 to-amber-700 text-white p-4 flex justify-between items-center">
+    <div class="flex items-center">
+      <img src="{{ asset('img/logo.png')}}" class="h-14">
+    </div>
+  
+    <nav class="space-x-4">
+      <a href="{{ route('siembra.create')}}" class="text-gray-300 hover:text-white">Siembra</a>
+      <a href="#" class="text-gray-300 hover:text-white">Terreno</a>
+      <a href="#" class="text-gray-300 hover:text-white">Fertilizacion</a>
+      <a href="#" class="text-gray-300 hover:text-white">Semilla</a>
+     <a href="#" class="text-black-300 border:bg-cyan-100 hover:text-white font-bold text-xl"></a>
+    </nav>
+  
+    <x-bladewind.button class="bg-gradient-to-r from-green-600 to-cyan-600 hover:bg-cyan-200 text-white px-4 py-2 rounded">Cerrar Sesión</x-bladewind.button>
+</header>
--- resources/views/agricultura/maiz/main.blade.php
+++ resources/views/agricultura/maiz/main.blade.php
@@ -2,14 +2,14 @@
 
 @section('app')
     {{-- Header --}}
-    {{-- @include('agricultura.maiz.layout.header') --}}
+    @include('agricultura.maiz.layout.header')
     <div class="flex">
         <div class="pt-5 flex-1">
             <h1 class="py-2 text-3xl text-center"> @yield('titulo-contenido') </h1>
             @yield('contenido')
         </div>
         {{-- Sidebar --}}
-        {{-- @include('agricultura.maiz.layout.sidebar') --}}
+        @include('agricultura.maiz.layout.sidebar')
     </div>
     {{-- Footer --}}
 @endsection
