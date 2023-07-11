539ffc2d4ca11cba7bc2ea72835a2f13ec0bfdcb
YJCQ <yjcq007@gmail.com>
2023-07-10 23:38:38 -0400

    agregar mostrar agricultura

--- resources/views/agricultura/maiz/semilla/mostrar.blade.php
+++ resources/views/agricultura/maiz/semilla/mostrar.blade.php
@@ -1,8 +1,8 @@
 @extends('main')
 
-@section('titulo', 'Riego')
+@section('titulo', 'Semilla')
 
-@section('titulo-contenido', 'Datos de Riego: '.$modelo->riego->)
+@section('titulo-contenido', 'Datos de Semilla: '.$modelo->ganado->identificacion)
 
 @section('contenido')
 <div>
@@ -10,8 +10,8 @@
         {{-- boton editar --}}
         <x-bladewind.button
             tag="a" 
-            size="tiny" href="{{ route('riego.edit', ['riego' => $modelo->id])}}"
-            class="bg-cyan-700"
+            size="tiny" href="{{ route('semilla.edit', ['semilla' => $modelo->id])}}"
+            class="bg-green-700"
         >Editar</x-bladewind.button>
         {{-- boton exportar --}}
         <x-bladewind.button
@@ -27,7 +27,7 @@
                 <div class="flex flex-row items-center py-3 justify-between">
                     <div>
                         <p class="flex text-xl">
-                            <span class="px-2">Siembra </span><span class="italic text-2xl font-light">{{$modelo->siembra->}}</span>
+                            <span class="px-2">Marca: </span><span class="italic text-2xl font-light">{{$modelo->marca}}</span>
                         </p>
                     </div>
                     <div>
@@ -35,31 +35,21 @@
                             <span class="px-2">Cantidad: </span><span class="italic text-2xl font-light">{{$modelo->cantidad}}</span>
                         </p>
                     </div>
-                    <div>
-                        <p class="flex text-xl">
-                            <span class="px-2">Frecuencia: </span><span class="italic text-2xl font-light">{{$modelo->frecuencia}}</span>
-                        </p>
-                    </div>
-                    <div>
-                        <p class="flex text-xl">
-                            <span class="px-2">Metodo: </span><span class="italic text-2xl font-light">{{$modelo->metodo}}</span>
-                        </p>
-                    </div>
                 </div>
             </div>
-            <h2 class="text-xl border-b border-t py-3 indent-3 border-gray-300 bg-cyan-700/10">Información Secundaria</h2>
+            <h2 class="text-xl border-b border-t py-3 indent-3 border-gray-300 bg-green-700/10">Información Secundaria</h2>
             <div class="p-3">
                 <div>
                     <x-bladewind.button
                         tag="a"
                         size="tiny"
-                        href="{{ route('bovino.control-sanitario.index', ['ganado' => $modelo->ganado->id])}}" 
+                        href="{{ route('maiz.control-sanitario.index', ['ganado' => $modelo->agricultura->id])}}" 
                     >Ver los controles sanitarios realizados</x-bladewind.button>
                 </div>
             </div>
         </div>
         <div  class="border-l-2 border-amber-700 border-dashed px-1">
-            <h2 class="text-lg text-white border-b border-t py-3 px-1 border-amber-700 bg-gradient-to-r from-cyan-700 to-amber-700">Información Adicional</h2>
+            <h2 class="text-lg text-white border-b border-t py-3 px-1 border-amber-700 bg-gradient-to-r from-green-700 to-amber-700">Información Adicional</h2>
         </div>
     </div>
 </div>
--- resources/views/agricultura/maiz/terreno/mostrar.blade.php
+++ resources/views/agricultura/maiz/terreno/mostrar.blade.php
@@ -1,8 +1,8 @@
 @extends('main')
 
-@section('titulo', 'Riego')
+@section('titulo', 'Terreno')
 
-@section('titulo-contenido', 'Datos de Riego: '.$modelo->riego->)
+@section('titulo-contenido', 'Datos de Terreno: '.$modelo->ganado->identificacion)
 
 @section('contenido')
 <div>
@@ -10,8 +10,8 @@
         {{-- boton editar --}}
         <x-bladewind.button
             tag="a" 
-            size="tiny" href="{{ route('riego.edit', ['riego' => $modelo->id])}}"
-            class="bg-cyan-700"
+            size="tiny" href="{{ route('terreno.edit', ['terreno' => $modelo->id])}}"
+            class="bg-green-700"
         >Editar</x-bladewind.button>
         {{-- boton exportar --}}
         <x-bladewind.button
@@ -27,39 +27,39 @@
                 <div class="flex flex-row items-center py-3 justify-between">
                     <div>
                         <p class="flex text-xl">
-                            <span class="px-2">Siembra </span><span class="italic text-2xl font-light">{{$modelo->siembra->}}</span>
+                            <span class="px-2">Hectareas: </span><span class="italic text-2xl font-light">{{$modelo->hectareas}}</span>
                         </p>
                     </div>
                     <div>
                         <p class="flex text-xl">
-                            <span class="px-2">Cantidad: </span><span class="italic text-2xl font-light">{{$modelo->cantidad}}</span>
+                            <span class="px-2">Ubicacion: </span><span class="italic text-2xl font-light">{{$modelo->ubicacion}}</span>
                         </p>
                     </div>
                     <div>
                         <p class="flex text-xl">
-                            <span class="px-2">Frecuencia: </span><span class="italic text-2xl font-light">{{$modelo->frecuencia}}</span>
+                            <span class="px-2">Tipo de suelo: </span><span class="italic text-2xl font-light">{{$modelo->tipo_suelo}}</span>
                         </p>
                     </div>
                     <div>
                         <p class="flex text-xl">
-                            <span class="px-2">Metodo: </span><span class="italic text-2xl font-light">{{$modelo->metodo}}</span>
+                            <span class="px-2">Nivel de humedad: </span><span class="italic text-2xl font-light">{{$modelo->nivel_humedad}}</span>
                         </p>
                     </div>
                 </div>
             </div>
-            <h2 class="text-xl border-b border-t py-3 indent-3 border-gray-300 bg-cyan-700/10">Información Secundaria</h2>
+            <h2 class="text-xl border-b border-t py-3 indent-3 border-gray-300 bg-green-700/10">Información Secundaria</h2>
             <div class="p-3">
                 <div>
                     <x-bladewind.button
                         tag="a"
                         size="tiny"
-                        href="{{ route('bovino.control-sanitario.index', ['ganado' => $modelo->ganado->id])}}" 
+                        href="{{ route('maiz.control-sanitario.index', ['ganado' => $modelo->agricultura->id])}}" 
                     >Ver los controles sanitarios realizados</x-bladewind.button>
                 </div>
             </div>
         </div>
         <div  class="border-l-2 border-amber-700 border-dashed px-1">
-            <h2 class="text-lg text-white border-b border-t py-3 px-1 border-amber-700 bg-gradient-to-r from-cyan-700 to-amber-700">Información Adicional</h2>
+            <h2 class="text-lg text-white border-b border-t py-3 px-1 border-amber-700 bg-gradient-to-r from-green-700 to-amber-700">Información Adicional</h2>
         </div>
     </div>
 </div>
